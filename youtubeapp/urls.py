411830e009a95a0e31a09a112148422ac913ad61
Muhammad Waqas Atta <waqaskhosa99@gmail.com>
2025-03-15 01:22:29 +0300

    Update urls.py

--- youtubeapp/urls.py
+++ youtubeapp/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
-from . import views  # Ensure views is imported
+from . import views
 
-app_name = 'youtubeapp'  # Ensure app_name is defined
+app_name = 'youtubeapp'
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('script_generation/', views.script_generation, name='script_generation'),
-    path('chat/', views.chat_with_deepseek, name='chat'),
     path('generate_script/', views.generate_script, name='generate_script'),
+    path('script_history/', views.get_script_history, name='script_history'),
+    path('chat/', views.chat_with_deepseek, name='chat_with_deepseek'),
+    path('script/<int:script_id>/', views.get_script, name='get_script'),
+    path('clear_script_history/', views.clear_script_history, name='clear_script_history'),
 ]
