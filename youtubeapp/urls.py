97ca81ce720376306bf8da18f95a7cc0db8924db
Muhammad Waqas Atta <waqaskhosa99@gmail.com>
2025-03-16 02:34:08 +0300

    Update urls.py

--- youtubeapp/urls.py
+++ youtubeapp/urls.py
@@ -4,11 +4,15 @@ from . import views
 app_name = 'youtubeapp'
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.home, name='home'),
+    path('chat/', views.index, name='index'),  # Chat functionality merged here
+    path('clear-chat-history/', views.clear_chat_history, name='clear_chat_history'),
+    path('youtube_analytics/', views.youtube_analytics, name='youtube_analytics'),
     path('script_generation/', views.script_generation, name='script_generation'),
     path('generate_script/', views.generate_script, name='generate_script'),
     path('script_history/', views.get_script_history, name='script_history'),
-    path('chat/', views.chat_with_deepseek, name='chat_with_deepseek'),
     path('script/<int:script_id>/', views.get_script, name='get_script'),
     path('clear_script_history/', views.clear_script_history, name='clear_script_history'),
+    path('contact/', views.contact_view, name='contact'),
+    path('update_region/', views.update_region, name='update_region'),
 ]
